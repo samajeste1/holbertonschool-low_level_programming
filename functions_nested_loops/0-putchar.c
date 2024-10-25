ad830a12e8f5fbcdacd06d0976bcf9e833798637
Your Name <you@example.com>
2024-10-25 09:17:27 +0000

    message

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar ('_');
-	_putchar ('p');
-	_putchar ('s');
-	_putchar ('w');
-	_putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

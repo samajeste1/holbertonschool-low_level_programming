dc6076c1644ddfada3dd82147d3588e6c51b316e
Your Name <you@example.com>
2025-06-27 13:51:04 +0000

    m

---
+++ pointers_arrays_strings/4-main.c
@@ -0,0 +1,8 @@
+// 4-main.c
+#include "main.h"
+
+int main(void)
+{
+	print_rev("Holberton!");
+	return (0);
+}
--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,38 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_rev - prints a string in reverse
- * @s: pointer to the string
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to be printed in reverse
+ *
+ * Return: void
  */
 void print_rev(char *s)
 {
 	int len = 0;
 
+	/* Find the length of the string */
 	while (s[len] != '\0')
-		len--;
+	{
+		len++;
+	}
 
-	for (len--; len >= 0; len--)
+	/* Print the string in reverse */
+	while (len > 0)
+	{
+		len--;
 		_putchar(s[len]);
+	}
+
 	_putchar('\n');
 }

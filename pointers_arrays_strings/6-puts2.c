904a7d94f121f083e479c7445b0785f943026a31
Your Name <you@example.com>
2025-07-06 19:45:52 +0000

    m

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string,
- *         starting with the first character
+ * puts2 - Prints every other character of a string
  * @str: The string to print
+ *
+ * Return: void
  */
 void puts2(char *str)
 {

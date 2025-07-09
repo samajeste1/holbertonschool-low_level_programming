db0950f959786076762e8fe3043b5ce2b4e5a0ab
Your Name <you@example.com>
2025-07-09 03:01:11 +0000

    m

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,26 +3,25 @@
 
 /**
  * create_array - creates an array of chars, and initializes it with a specific char
- * @size: size of the array
- * @c: char to initialize the array with
+ * @size: The size of the array to create
+ * @c: The character to initialize the array with
  *
- * Return: pointer to the array, or NULL if it fails or size is 0
+ * Return: Pointer to the array, or NULL if it fails or size is 0
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
+	char *arr;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    arr = malloc(size * sizeof(char));
-    if (arr == NULL)
-        return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = c;
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 
-    return (arr);
+	return (arr);
 }
-

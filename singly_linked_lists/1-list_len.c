d572040306d929dc2afd51ce49b62cf7d8aeadeb
root <you@example.com>
2025-08-01 21:24:32 +0000

    m

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,12 @@
+size_t list_len(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+void free_list(list_t *head)
+{
+    list_t *tmp;
+
+    while (head)
+    {
+        tmp = head->next;
+        free(head->str);
+        free(head);
+        head = tmp;
+    }
+}

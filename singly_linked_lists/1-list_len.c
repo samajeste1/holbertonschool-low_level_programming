c64a6493b3fcb89782de3719ec97fefb82caac49
Your Name <you@example.com>
2025-08-05 22:26:35 +0000

    m

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,12 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - counts the number of elements in a linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of elements in the list
+ */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
 
-    return (count);
+	return (count);
 }
